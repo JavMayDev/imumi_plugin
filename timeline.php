349f5d1820b2566b87a9e67d27fe169d455c4ed2
javi_mac <javimayorque@gmail.com>
2021-01-07 14:48:07 -0600

    create hardcoded static page with fopen

--- timeline.php
+++ timeline.php
@@ -9,9 +9,14 @@ include(__DIR__.'/admin/index.php');
 include(__DIR__.'/create_table.php');
 register_activation_hook(__FILE__, 'tl_create_tables');
 
-/* include(__DIR__.'/create_static_page.php'); */
-/* register_activation_hook(__FILE__, 'tl_create_static_page'); */
-/* add_action('wp', 'tl_update_static_page'); */
+function tl_create_page (){
+    $file = fopen(get_template_directory().'/page-timeline.php', 'wb');
+    fwrite($file, '<?php include(realpath(__DIR__.\'/../../plugins/timeline/public/index.php\')) ?>');
+    fclose($file);
+}
+
+register_activation_hook(__FILE__, 'tl_create_page');
+
 
 
 ?>
